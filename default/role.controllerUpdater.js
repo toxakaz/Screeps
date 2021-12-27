b1663b108f2946179d96f81a56472a12e70f9236
ToxaKaz <anton.a.kazancev@gmail.com>
2021-12-27 23:10:00 +0700

    commit

--- default/role.controllerUpdater.js
+++ default/role.controllerUpdater.js
@@ -36,10 +36,10 @@ var roleControllerUpdater =
             if(dif > 0)
             {
                 if (creep.withdraw(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-                    creep.moveTo(spawn);
+                    creep.moveTo(spawn, {reusePath: 0});
             }
             else
-                creep.moveTo(Game.flags.AFK);
+                creep.moveTo(Game.flags.AFK, {reusePath: 0});
 
             if (creep.store.getFreeCapacity(RESOURCE_ENERGY) == 0)
                 creep.memory.fill = false;
@@ -48,7 +48,7 @@ var roleControllerUpdater =
         {
             var controler = decode.room(creep).controller;
             if (creep.upgradeController(controler) == ERR_NOT_IN_RANGE)
-                creep.moveTo(controler);
+                creep.moveTo(controler, {reusePath: 0});
             if (creep.store.getUsedCapacity(RESOURCE_ENERGY) == 0)
                 creep.memory.fill = true;
         }
